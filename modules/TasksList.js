b700eacfb075756068d5a3184a46f2c1573851d9
Ciragane Nicole <ciraganenicole@gmail.com>
2022-03-28 15:39:23 +0200

    Update TasksList.js

--- modules/TasksList.js
+++ modules/TasksList.js
@@ -37,19 +37,9 @@ class Tasks {
         this.setHtml();
         this.setup();
     }
-    hamburger = () => {
-        const remove = document.querySelectorAll('.remove');
-        remove.forEach((rmBtn) => {
-            const hamb = document.getElementById(rmBtn.id.replace('remove', 'myLinks'));
-            rmBtn.addEventListener('click', () => {
-                rmBtn.style.display = 'none';
-                hamb.style.display = 'flex';
-            });
-        });
-    };
-
+ 
     setupRemove() {
-         hamburger = () => {
+         const hamburger = () => {
         const remove = document.querySelectorAll('.remove');
         remove.forEach((rmBtn) => {
             const hamb = document.getElementById(rmBtn.id.replace('remove', 'myLinks'));
