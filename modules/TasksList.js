3bcea602e25d3da1dbcf7e72343197dc6a80c59d
Ciragane Nicole <ciraganenicole@gmail.com>
2022-03-30 17:15:08 +0200

    Set up testing environment

--- modules/TasksList.js
+++ modules/TasksList.js
@@ -37,7 +37,7 @@ class Tasks {
         this.setHtml();
         this.setup();
     }
-    hamburger = () => {
+    hamburger() {
         const remove = document.querySelectorAll('.remove');
         remove.forEach((rmBtn) => {
             const hamb = document.getElementById(rmBtn.id.replace('remove', 'myLinks'));
